04e8355850ed8016870bbe1c55932fa5e155ff07
Jonathan Terrell <terrell.jm@gmail.com>
2024-06-21 11:07:21 +0200

    v0.1.441

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,8 @@ export default class DefaultPresentor implements IPresentor {
     async render(id: string, renderTo: string | HTMLElement): Promise<void> {
         const url = `https://firebasestorage.googleapis.com/v0/b/datapos-prod.appspot.com/o/connectors%2Fhighcharts-Dl4Gk9bH.js?alt=media`;
         const Highcharts = await import(url);
+        console.log(Highcharts);
+        console.log(Highcharts.default);
         new Highcharts.Chart(renderTo, {
             chart: { type: 'bar' },
             title: { text: 'Fruit Consumption' },
