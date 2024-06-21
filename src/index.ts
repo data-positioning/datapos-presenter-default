715e77e638ac3665f3a9d22752b1c12a49e8f080
Jonathan Terrell <terrell.jm@gmail.com>
2024-06-21 18:22:19 +0200

    v0.1.479

--- src/index.ts
+++ src/index.ts
@@ -3,31 +3,8 @@ import type { Axis, ChartCallbackFunction, ChartOptions, LegendOptions, Options,
 import type { IPresenter, IPresenterConfig, IPresenterItemConfig } from '@datapos/datapos-share-core';
 // import type { Series, SeriesAreaOptions, SeriesBarOptions, SeriesColumnOptions, SeriesLineOptions } from 'highcharts';
 
-// // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// // @ts-expect-error
-// import ColumnSeries from 'highcharts/es-modules/Series/Column/ColumnSeries.js';
-
-// // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// // @ts-expect-error
-// import LineSeries from 'highcharts/es-modules/Series/Line/LineSeries.js';
-
-// // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// // @ts-expect-error
-// import Chart from 'highcharts/es-modules/Core/Chart/Chart.js';
-// // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-
-// // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// // @ts-expect-error
-// import BarSeries from 'highcharts/es-modules/Series/Bar/BarSeries.js';
-// console.log(BarSeries);
-
 // Dependencies - Data
 import config from './config.json';
-
-// Interfaces/Types - Module Import
-interface ModuleImport {
-    default: unknown;
-}
 
 // Classes - Default Presentation Set
 export default class DefaultPresenter implements IPresenter {
@@ -52,17 +29,6 @@ export default class DefaultPresenter implements IPresenter {
     }
 
     async render(id: string, renderTo: string | HTMLElement): Promise<void> {
-        // // TODO: See: https://www.highcharts.com/docs/getting-started/installation-with-esm for bundling option.
-        // const chartUrl = 'https://code.highcharts.com/es-modules/Core/Chart/Chart.js';
-        // const Chart = ((await import(chartUrl)) as ModuleImport).default as typeof HighchartsChart;
-        // const barUrl = 'https://code.highcharts.com/es-modules/Series/Bar/BarSeries.js';
-        // ((await import(barUrl)) as ModuleImport).default as typeof Series;
-
-        // const url = 'https://datapos-plugins.pages.dev/vendor-highcharts.js';
-        // const Highcharts = ((await import(url)) as ModuleImport).default;
-
-        // console.log(Highcharts);
-
         const Highcharts = (await import('highcharts')).default;
 
         new Highcharts.Chart(renderTo, {
