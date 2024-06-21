a5199aff4cc91c55afb924afc487ba6a688de0e7
Jonathan Terrell <terrell.jm@gmail.com>
2024-06-21 11:19:48 +0200

    v0.1.447

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ export default class DefaultPresentor implements IPresentor {
     async render(id: string, renderTo: string | HTMLElement): Promise<void> {
         // const url = `https://firebasestorage.googleapis.com/v0/b/datapos-prod.appspot.com/o/connectors%2Fhighcharts-Dl4Gk9bH.js?alt=media`;
         const url = 'https://code.highcharts.com/es-modules/Core/Chart/Chart.js';
-        const Highcharts = await import(url);
+        const Highcharts = (await import(url)).default;
         console.log(Highcharts);
         // console.log(Highcharts.h.default);
         new Highcharts.Chart(renderTo, {
