fb5eba536814cf066492c7f74b46e5b6f79b85ca
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-10-31 23:07:44 +0100

    v0.1.640

--- src/sampleData/useSampleData.ts
+++ src/sampleData/useSampleData.ts
@@ -1,5 +1,27 @@
 // Dependencies
 import headcountForCalendarYear from './headcountForCalendarYear.json';
 
-// Composables
-export function useSampleData() {}
+// Types
+type MonthData = (typeof headcountForCalendarYear.months)[number];
+type MeasureKey = keyof MonthData;
+
+// Non-Reactive Variables
+const measureValues: Record<string, [number, number?][]> = {};
+
+// Composables - Use sample data.
+export function useSampleData() {
+    function getMeasureValues(id: MeasureKey): [number, number?][] {
+        const values = measureValues[id];
+        if (values) return values;
+        return calcMeasureValues(id);
+    }
+
+    // Utilities - Calculate measure values.
+    function calcMeasureValues(id: MeasureKey): [number, number?][] {
+        measureValues[id] = headcountForCalendarYear.months.map((month) => [month[id]]);
+        return measureValues[id];
+    }
+
+    // Exposures
+    return { getMeasureValues };
+}
