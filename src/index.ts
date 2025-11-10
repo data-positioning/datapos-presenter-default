fe057859959a9916b023b99a2860763c4d3a7cea
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-11-10 19:43:32 +0100

    v0.1.746

--- src/index.ts
+++ src/index.ts
@@ -182,86 +182,86 @@ export default class DefaultPresenter implements Presenter {
         // const html = markdownParser.render(processedMarkdown);
         // renderTo.innerHTML = html;
 
-        for (const visualElements of renderTo.querySelectorAll('.datapos-visual')) {
-            const datasetOptions = decodeURIComponent((visualElements as HTMLElement).dataset.options);
-            try {
-                const visualConfig = JSON.parse(datasetOptions) as PresentationVisualConfig;
+        // for (const visualElements of renderTo.querySelectorAll('.datapos-visual')) {
+        //     const datasetOptions = decodeURIComponent((visualElements as HTMLElement).dataset.options);
+        //     try {
+        //         const visualConfig = JSON.parse(datasetOptions) as PresentationVisualConfig;
 
-                if (!data) {
-                    for (const measure of visualConfig.content.data.measures) {
-                        measure.data = this.sampleData.getMeasureValues([measure.id]);
-                    }
-                }
+        //         if (!data) {
+        //             for (const measure of visualConfig.content.data.measures) {
+        //                 measure.data = this.sampleData.getMeasureValues([measure.id]);
+        //             }
+        //         }
 
-                const tabBarElement = document.createElement('div');
-                tabBarElement.className = 'dp-tab-bar';
-                const viewContainerElement = document.createElement('div');
-                let defaultViewType: PresentationVisualViewType | undefined = undefined;
-                for (const viewConfig of visualConfig.views) {
-                    const viewCategoryId = viewConfig.categoryId;
-                    switch (viewCategoryId) {
-                        case 'cartesian': {
-                            const cartesianViewConfig = viewConfig as PresentationVisualCartesianViewConfig;
-                            const viewType = presentationViewTypeMap[`${viewCategoryId}_${cartesianViewConfig.typeId}`] as PresentationVisualCartesianViewType;
-                            if (!defaultViewType || cartesianViewConfig.default) defaultViewType = viewType;
-                            const element = document.createElement('div');
-                            element.textContent = viewType.label['en-gb'];
-                            element.addEventListener('click', () => this.highcharts.renderCartesianChart(viewType, visualConfig.content, viewContainerElement));
-                            tabBarElement.appendChild(element);
-                            break;
-                        }
-                        case 'polar': {
-                            const polarViewConfig = viewConfig as PresentationVisualPolarViewConfig;
-                            const viewType = presentationViewTypeMap[`${viewCategoryId}_${polarViewConfig.typeId}`] as PresentationVisualPolarViewType;
-                            if (!defaultViewType || polarViewConfig.default) defaultViewType = viewType;
-                            const element = document.createElement('div');
-                            element.textContent = viewType.label['en-gb'];
-                            element.addEventListener('click', () => this.highcharts.renderPolarChart(viewType, visualConfig.content, viewContainerElement));
-                            tabBarElement.appendChild(element);
-                            break;
-                        }
-                        case 'range': {
-                            const rangeViewConfig = viewConfig as PresentationVisualRangeViewConfig;
-                            const viewType = presentationViewTypeMap[`${viewCategoryId}_${rangeViewConfig.typeId}`] as PresentationVisualRangeViewType;
-                            if (!defaultViewType || rangeViewConfig.default) defaultViewType = viewType;
-                            const element = document.createElement('div');
-                            element.textContent = viewType.label['en-gb'];
-                            element.addEventListener('click', () => this.highcharts.renderRangeChart(viewType, visualConfig.content, viewContainerElement));
-                            tabBarElement.appendChild(element);
-                            break;
-                        }
-                        case 'values': {
-                            const valuesViewConfig = viewConfig as PresentationVisualValuesViewConfig;
-                            const viewType = presentationViewTypeMap[viewCategoryId] as PresentationVisualValuesViewType;
-                            if (!defaultViewType) defaultViewType = viewType;
-                            const element = document.createElement('div');
-                            element.textContent = viewType.label['en-gb'];
-                            element.addEventListener('click', () => this.dataTable.render(viewType, visualConfig.content, viewContainerElement));
-                            tabBarElement.appendChild(element);
-                            break;
-                        }
-                    }
-                }
-                visualElements.appendChild(tabBarElement);
-                visualElements.appendChild(viewContainerElement);
-                switch (defaultViewType.categoryId) {
-                    case 'cartesian':
-                        this.highcharts.renderCartesianChart(defaultViewType as PresentationVisualCartesianViewType, visualConfig.content, viewContainerElement);
-                        break;
-                    case 'polar':
-                        this.highcharts.renderPolarChart(defaultViewType as PresentationVisualPolarViewType, visualConfig.content, viewContainerElement);
-                        break;
-                    case 'range':
-                        this.highcharts.renderRangeChart(defaultViewType as PresentationVisualRangeViewType, visualConfig.content, viewContainerElement);
-                        break;
-                    case 'values':
-                        this.dataTable.render(defaultViewType as PresentationVisualValuesViewType, visualConfig.content, viewContainerElement);
-                        break;
-                }
-            } catch (error) {
-                console.error(error);
-                visualElements.textContent = 'Invalid options.';
-            }
-        }
+        //         const tabBarElement = document.createElement('div');
+        //         tabBarElement.className = 'dp-tab-bar';
+        //         const viewContainerElement = document.createElement('div');
+        //         let defaultViewType: PresentationVisualViewType | undefined = undefined;
+        //         for (const viewConfig of visualConfig.views) {
+        //             const viewCategoryId = viewConfig.categoryId;
+        //             switch (viewCategoryId) {
+        //                 case 'cartesian': {
+        //                     const cartesianViewConfig = viewConfig as PresentationVisualCartesianViewConfig;
+        //                     const viewType = presentationViewTypeMap[`${viewCategoryId}_${cartesianViewConfig.typeId}`] as PresentationVisualCartesianViewType;
+        //                     if (!defaultViewType || cartesianViewConfig.default) defaultViewType = viewType;
+        //                     const element = document.createElement('div');
+        //                     element.textContent = viewType.label['en-gb'];
+        //                     element.addEventListener('click', () => this.highcharts.renderCartesianChart(viewType, visualConfig.content, viewContainerElement));
+        //                     tabBarElement.appendChild(element);
+        //                     break;
+        //                 }
+        //                 case 'polar': {
+        //                     const polarViewConfig = viewConfig as PresentationVisualPolarViewConfig;
+        //                     const viewType = presentationViewTypeMap[`${viewCategoryId}_${polarViewConfig.typeId}`] as PresentationVisualPolarViewType;
+        //                     if (!defaultViewType || polarViewConfig.default) defaultViewType = viewType;
+        //                     const element = document.createElement('div');
+        //                     element.textContent = viewType.label['en-gb'];
+        //                     element.addEventListener('click', () => this.highcharts.renderPolarChart(viewType, visualConfig.content, viewContainerElement));
+        //                     tabBarElement.appendChild(element);
+        //                     break;
+        //                 }
+        //                 case 'range': {
+        //                     const rangeViewConfig = viewConfig as PresentationVisualRangeViewConfig;
+        //                     const viewType = presentationViewTypeMap[`${viewCategoryId}_${rangeViewConfig.typeId}`] as PresentationVisualRangeViewType;
+        //                     if (!defaultViewType || rangeViewConfig.default) defaultViewType = viewType;
+        //                     const element = document.createElement('div');
+        //                     element.textContent = viewType.label['en-gb'];
+        //                     element.addEventListener('click', () => this.highcharts.renderRangeChart(viewType, visualConfig.content, viewContainerElement));
+        //                     tabBarElement.appendChild(element);
+        //                     break;
+        //                 }
+        //                 case 'values': {
+        //                     const valuesViewConfig = viewConfig as PresentationVisualValuesViewConfig;
+        //                     const viewType = presentationViewTypeMap[viewCategoryId] as PresentationVisualValuesViewType;
+        //                     if (!defaultViewType) defaultViewType = viewType;
+        //                     const element = document.createElement('div');
+        //                     element.textContent = viewType.label['en-gb'];
+        //                     element.addEventListener('click', () => this.dataTable.render(viewType, visualConfig.content, viewContainerElement));
+        //                     tabBarElement.appendChild(element);
+        //                     break;
+        //                 }
+        //             }
+        //         }
+        //         visualElements.appendChild(tabBarElement);
+        //         visualElements.appendChild(viewContainerElement);
+        //         switch (defaultViewType.categoryId) {
+        //             case 'cartesian':
+        //                 this.highcharts.renderCartesianChart(defaultViewType as PresentationVisualCartesianViewType, visualConfig.content, viewContainerElement);
+        //                 break;
+        //             case 'polar':
+        //                 this.highcharts.renderPolarChart(defaultViewType as PresentationVisualPolarViewType, visualConfig.content, viewContainerElement);
+        //                 break;
+        //             case 'range':
+        //                 this.highcharts.renderRangeChart(defaultViewType as PresentationVisualRangeViewType, visualConfig.content, viewContainerElement);
+        //                 break;
+        //             case 'values':
+        //                 this.dataTable.render(defaultViewType as PresentationVisualValuesViewType, visualConfig.content, viewContainerElement);
+        //                 break;
+        //         }
+        //     } catch (error) {
+        //         console.error(error);
+        //         visualElements.textContent = 'Invalid options.';
+        //     }
+        // }
     }
 }
