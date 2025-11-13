172e8ee9b5c2593920939bec3a4b288c3df78e03
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-11-13 22:59:39 +0100

    v0.1.909

--- src/index.ts
+++ src/index.ts
@@ -163,7 +163,7 @@ export default class DefaultPresenter implements Presenter {
 
         const toolModuleConfig = this.toolModuleConfigs.find((config) => (config.id = 'datapos-tool-highcharts'));
         console.log('datapos-tool-highcharts', toolModuleConfig);
-        const url = 'https://engine-eu.datapos.app/tools/v0.0.21/datapos-tool-highcharts.es.js';
+        const url = `https://engine-eu.datapos.app/tools/${toolModuleConfig.version}/datapos-tool-highcharts.es.js`;
         const HighchartsTool = (await import(/* @vite-ignore */ url)).HighchartsTool as new () => HighchartsTool;
         this.highchartsTool = new HighchartsTool();
     }
@@ -174,7 +174,7 @@ export default class DefaultPresenter implements Presenter {
 
         const toolModuleConfig = this.toolModuleConfigs.find((config) => (config.id = 'datapos-tool-micromark'));
         console.log('datapos-tool-micromark', toolModuleConfig);
-        const url = 'https://engine-eu.datapos.app/tools/v0.1.887/datapos-tool-micromark.es.js';
+        const url = `https://engine-eu.datapos.app/tools/${toolModuleConfig.version}/datapos-tool-micromark.es.js';
         const MicromarkToolConstructor = (await import(/* @vite-ignore */ url)).MicromarkTool as new () => MicromarkTool;
         this.micromarkTool = new MicromarkToolConstructor();
     }
