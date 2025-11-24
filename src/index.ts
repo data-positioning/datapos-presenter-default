435b4a82e9c2687cca6e98e0154d14b6042d799a
Jonathan Terrell <jonathanterrell@Jonathans-Mac-mini.local>
2025-11-24 12:09:56 +0100

    v0.1.985

--- src/index.ts
+++ src/index.ts
@@ -66,7 +66,7 @@ export default class DefaultPresenter implements Presenter {
 
         // Render markdown to HTML
         this.micromarkTool = await this.loadMicromarkTool();
-        const html = await this.micromarkTool.render(processedMarkdown);
+        const html = await this.micromarkTool.render(processedMarkdown, { tables: true });
         renderTo.innerHTML = html;
         this.micromarkTool.highlight();
 
